7a1b1560c49586fb94bc1135710bfc73226381a3
basskibo <jageticb@yahoo.com>
2017-04-12 19:30:15 +0200

    saasfa

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class obuka extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native Obuka !
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
