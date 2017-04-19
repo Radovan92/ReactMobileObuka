2648451a6af531ba9f482d16af34eeb463aefc43
Dragan <draganspancicnovisad@gmail.com>
2017-04-19 19:29:20 +0200

    Pera

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class obuka extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native Obuka !
+          asdfg !
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
