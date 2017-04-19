1db42f9cc3349c0a1c8a391fd5d9806239a8009b
stevanvasiljevic <stevanvasiljevic.pmf@gmail.com>
2017-04-19 19:18:56 +0200

    Promenjen naslov

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class obuka extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native Obuka !
+          Neki Naslov !
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
